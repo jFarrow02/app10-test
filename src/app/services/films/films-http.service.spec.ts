6e08a720066b7d9757a5cd980f22b752e5682f15
Jack Farrow <jfarrow@mbp-jfarrow.local>
2020-01-29 10:56:48 -0500

    Tests getFilmsByUrl HAPPY PATH ok

--- src/app/services/films/films-http.service.spec.ts
+++ src/app/services/films/films-http.service.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClient } from '@angular/common/http';
 import { FilmsHttpService } from './films-http.service';
+import { of } from 'rxjs';
 
 describe('FilmsHttpService', () => {
-  let httpClientSpy: jasmine.Spy;
+  let httpClientSpy: { get: jasmine.Spy };
   let filmsHttpSrvc: FilmsHttpService;
+  const MOCK_DATA = [ { title: 'The Force Woke Up' } ];
+  const MOCK_URL = 'https://foo.bar/baz';
 
   beforeEach( () => {
 
     TestBed.configureTestingModule(
       {
-        
+        providers: [
+          FilmsHttpService,
+          { provide: HttpClient, useValue: httpClientSpy },
+        ]
       }
     );  //end configureTestingModule
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
@@ -20,4 +26,15 @@ describe('FilmsHttpService', () => {
   it('should be created', () => {
     expect(filmsHttpSrvc).toBeTruthy();
   });
+
+  it('should GET via http client', () => {
+    httpClientSpy.get.and.returnValue( of( MOCK_DATA ) );
+
+    filmsHttpSrvc.getFilmsByUrl(MOCK_URL)
+      .subscribe(
+        data => {
+          expect( data ).toEqual( MOCK_DATA )
+        }
+      )
+  })
 });
