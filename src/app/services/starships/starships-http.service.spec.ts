3a086e10ea74fbf3c2d0ea797321bb3db93e318f
Jack Farrow <jfarrow@mbp-jfarrow.local>
2020-01-29 13:33:10 -0500

    Adds http client dependency to starships http service

--- src/app/services/starships/starships-http.service.spec.ts
+++ src/app/services/starships/starships-http.service.spec.ts
@@ -1,20 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
 import { StarshipsHttpService } from './starships-http.service';
 
 describe('StarshipsHttpService', () => {
 
   let starshipHttpSrvc: StarshipsHttpService;
+  let httpClientMock: { get: jasmine.Spy };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule(
-      {
-
-      }
-    )
-
-    starshipHttpSrvc = new StarshipsHttpService();
-  });
+  beforeEach( async(
+    () => {
+      TestBed.configureTestingModule(
+        {
+          providers: [
+            StarshipsHttpService,
+            { provide: HttpClient, useValue: httpClientMock },
+          ]
+        }
+      )
+      
+      httpClientMock = jasmine.createSpyObj('HttpClient', ['get'])
+      starshipHttpSrvc = new StarshipsHttpService(<any>httpClientMock);
+    }
+  ));
 
   it('should be created', () => {
     expect(starshipHttpSrvc).toBeDefined();
