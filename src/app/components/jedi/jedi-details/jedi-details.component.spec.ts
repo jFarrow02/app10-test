b510fc94010d2f497949f78b10fe7e2aceb10bc7
Jack Farrow <jfarrow@mbp-jfarrow.local>
2020-01-31 16:54:37 -0500

    adds debug element to jedi-details spec; ALL TESTS PASS

--- src/app/components/jedi/jedi-details/jedi-details.component.spec.ts
+++ src/app/components/jedi/jedi-details/jedi-details.component.spec.ts
@@ -1,23 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JediDetailsComponent } from './jedi-details.component';
+import { By } from '@angular/platform-browser';
 
 describe('JediDetailsComponent', () => {
   let component: JediDetailsComponent;
   let fixture: ComponentFixture<JediDetailsComponent>;
-
+  let debugEl;
+  let jediEl;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ JediDetailsComponent ]
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+    
     fixture = TestBed.createComponent(JediDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    debugEl = fixture.debugElement.query(By.css('.jedi-details>h4'))
+    jediEl = debugEl.nativeElement;
+  }));
+
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(JediDetailsComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
   it('should create', () => {
     expect(component).toBeTruthy();
